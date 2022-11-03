5f05cd47289e4926391592249815fc71788e478e
Gustavo Silva <gusttavo.x.santos@gmail.com>
2022-11-03 20:49:06 -0300

    test: garantido que CreateUserUseCase retorne lance um erro caso usuario já exista

---
+++ src/application/useCases/CreateUser.spec.ts
@@ -0,0 +1,25 @@
+import { UsersRepository } from '../contracts/users-repository';
+import { CreateUserUseCase } from './CreateUser';
+
+describe('Create user useCase Tests', () => {
+  it('Should throw a exception with User already exists', async () => {
+    const usersRepository: jest.Mocked<UsersRepository> = {
+      findByEmail: jest.fn().mockResolvedValueOnce({
+        id: 1,
+        name: 'any_name',
+        email: 'any_email@email.com',
+        role: 'customer'
+      })
+    };
+
+    const sut = new CreateUserUseCase(usersRepository);
+
+    const promise = sut.create({
+      name: 'any_name',
+      email: 'any_email@email.com',
+      password: 'any_password'
+    });
+
+    await expect(promise).rejects.toThrow(Error);
+  });
+});
