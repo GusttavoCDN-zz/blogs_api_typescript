30df7ded57ef68adcdf0ced53e2c25c976712517
Gustavo Silva <gusttavo.x.santos@gmail.com>
2022-11-03 18:49:55 -0300

    chore: projeto iniciado

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import { Config } from 'jest';
+
+const config: Config = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  preset: 'ts-jest',
+  testEnvironment: "node",
+};
+
+export default config;
