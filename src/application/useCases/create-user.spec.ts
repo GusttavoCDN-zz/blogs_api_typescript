9348282e8d7c3a1514bbc8d3d35304694f018aab
Gustavo Silva <gusttavo.x.santos@gmail.com>
2022-11-03 21:46:09 -0300

    test: garantido que usuario sera criado caso não exista no db

--- src/application/useCases/create-user.spec.ts
+++ src/application/useCases/create-user.spec.ts
@@ -1,25 +1,71 @@
 import { UsersRepository } from '../contracts/users-repository';
 import { CreateUserUseCase } from './create-user';
 
+const createdUserResponse = {
+  id: 1,
+  name: 'any_name',
+  email: 'any_email@email.com',
+  role: 'customer'
+};
+
+const createUserRequest = {
+  name: 'any_name',
+  email: 'any_email@email.com',
+  password: 'any_password'
+};
+
+interface SutTypes {
+  sut: CreateUserUseCase
+  usersRepository: jest.Mocked<UsersRepository>
+}
+
+const makeSut = (): SutTypes => {
+  const usersRepository: jest.Mocked<UsersRepository> = {
+    findByEmail: jest.fn().mockResolvedValue(null),
+    create: jest.fn().mockResolvedValue(createdUserResponse)
+  };
+
+  const sut = new CreateUserUseCase(usersRepository);
+
+  return { sut, usersRepository };
+};
+
 describe('Create user useCase Tests', () => {
   it('Should throw a exception with User already exists', async () => {
-    const usersRepository: jest.Mocked<UsersRepository> = {
-      findByEmail: jest.fn().mockResolvedValueOnce({
-        id: 1,
-        name: 'any_name',
-        email: 'any_email@email.com',
-        role: 'customer'
-      })
-    };
+    const { sut, usersRepository } = makeSut();
 
-    const sut = new CreateUserUseCase(usersRepository);
+    usersRepository.findByEmail.mockResolvedValueOnce(createdUserResponse);
 
-    const promise = sut.create({
-      name: 'any_name',
-      email: 'any_email@email.com',
-      password: 'any_password'
-    });
+    const promise = sut.create(createUserRequest);
 
     await expect(promise).rejects.toThrow(Error);
+  });
+
+  it('Should call findByEmail with correct value', async () => {
+    const { sut, usersRepository } = makeSut();
+
+    const findByEmailSpy = jest.spyOn(usersRepository, 'findByEmail');
+
+    await sut.create(createUserRequest);
+
+    expect(findByEmailSpy).toHaveBeenCalledWith(createUserRequest.email);
+  });
+
+  it('Should return a user on success', async () => {
+    const { sut } = makeSut();
+
+    const user = await sut.create(createUserRequest);
+
+    expect(user).toEqual(createdUserResponse);
+  });
+
+  it('Should call create with correct values', async () => {
+    const { sut, usersRepository } = makeSut();
+
+    const createSpy = jest.spyOn(usersRepository, 'create');
+
+    await sut.create(createUserRequest);
+
+    expect(createSpy).toHaveBeenCalledWith(createUserRequest);
   });
 });
