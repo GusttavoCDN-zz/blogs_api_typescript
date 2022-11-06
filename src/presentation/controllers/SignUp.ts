ca5500c26d1c49b76527df515c58d57f34d451e9
Gustavo Silva <gusttavo.x.santos@gmail.com>
2022-11-06 12:00:49 -0300

    feat: garantido que seja signUp retorne 201 caso dados sejam validos

--- src/presentation/controllers/SignUp.ts
+++ src/presentation/controllers/SignUp.ts
@@ -1,9 +1,14 @@
 import { CreatedUserDTO, CreateUserDTO } from "../../application/DTO's/user-dtos";
+import { CreateUserUseCase } from '../../application/useCases/create-user';
 import { Controller, HttpResponse, HttpResquest } from '../contracts';
 import { SignUpValidator } from '../contracts/SignUpValidator';
 
 export class SignUp implements Controller {
-  constructor(private readonly requestValidator: SignUpValidator) {}
+  constructor(
+    private readonly requestValidator: SignUpValidator,
+    private readonly createUser: CreateUserUseCase
+  ) {}
+
   public handle = async (
     request: HttpResquest<CreateUserDTO>
   ): Promise<HttpResponse<CreatedUserDTO>> => {
@@ -16,6 +21,11 @@ export class SignUp implements Controller {
       };
     }
 
-    return null as any;
+    const output = await this.createUser.execute(request.body);
+
+    return {
+      statusCode: 201,
+      body: output
+    };
   };
 }
